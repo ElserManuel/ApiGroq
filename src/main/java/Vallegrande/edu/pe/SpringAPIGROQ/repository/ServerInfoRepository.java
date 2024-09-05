4ff69e140b7f98a1688cf734dcd3fb95b1086f15
Elser Manuel Lazaro Rosa <88794669+ElserManuel@users.noreply.github.com>
2024-09-05 13:50:03 +0000

    dea

--- src/main/java/Vallegrande/edu/pe/SpringAPIGROQ/repository/ServerInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package Vallegrande.edu.pe.SpringAPIGROQ.repository;
-
-import Vallegrande.edu.pe.SpringAPIGROQ.Model.ServerInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ServerInfoRepository  extends MongoRepository<ServerInfo, String> {
-}
